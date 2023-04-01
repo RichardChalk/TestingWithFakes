02b51502e7151341f268dbf9c6349462a65ae643
Richard Chalk <barmybrit@gmail.com>
2023-04-01 07:27:16 +0200

    Add project files.

---
+++ TestingWithFakes/Services/RegistrationService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingWithFakes.Services
+{
+    public class RegistrationService : IRegistrationService
+    {
+        private readonly IUserRepository _userRepository;
+        public RegistrationService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public RegistrationStatus Register(string email)
+        {
+            if (AlreadyRegistered(email)) 
+                return RegistrationStatus.AlreadyRegistered;
+            return RegistrationStatus.Ok;
+        }
+
+        public bool AlreadyRegistered(string email)
+        {
+            return _userRepository.UserExists(email);
+        }
+    }
+}
